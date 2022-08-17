d9da7891e69334e465cf2779f39df5cabea917e8
juliatruf <julia.trufanova@bk.ru>
2022-08-17 11:44:02 +0300

    Adding a test for the 'Bin' button

---
+++ test_items.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+import time
+
+def is_element_present(browser):
+    try:
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        browser.get(link)
+        time.sleep(30)
+        browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+        return True
+    except:
+        return False
+
+def test_user_should_see_bin_button(browser):
+    assert is_element_present(browser)==True, "There is no 'Bin' button"
